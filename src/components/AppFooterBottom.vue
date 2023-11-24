93e7dd3ca1826e20f9a604ee2200da5af7bc421a
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2023-11-24 15:46:03 +0100

    add: icon section

---
+++ src/components/AppFooterBottom.vue
@@ -0,0 +1,70 @@
+<script>
+export default {
+    data() {
+        return {
+            footerSocial: [
+                'footer-facebook.png',
+                'footer-twitter.png',
+                'footer-youtube.png',
+                'footer-periscope.png',
+                'footer-pinterest.png',
+            ]
+        }
+    },
+    methods: {
+        getImageUrl(imgName) {
+            return new URL(`../assets/img/${imgName}`, import.meta.url).href;
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="wrapper">
+        <div class="container">
+            <div>
+                <span class="button"><a href="">sing-up now!</a></span>
+            </div>
+            <div class="social-container">
+                <span>follow us</span>
+                <div v-for="(image, index) in footerSocial">
+                    <img :src="getImageUrl(image)" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../style/partials/mixin" as *;
+@use "../style/partials/variables" as *;
+
+.wrapper {
+    background-color: $footer;
+    position: relative;
+    padding: 2rem;
+
+    .container {
+        @include flex(row, space-between, center);
+
+        span {
+            color: $primary;
+        }
+
+        .button {
+            color: white;
+            padding: .5rem;
+            border: 3px solid $primary;
+            a {
+                text-decoration: none;
+                color: inherit;
+            }
+        }
+
+        .social-container {
+            @include flex(row, center, center);
+            gap: 1rem;
+        }
+    }
+}
+</style>
---
+++ src/components/InfoBanner.vue
@@ -0,0 +1,74 @@
+<script>
+export default {
+    data() {
+        return {
+            icons: [
+                {
+                    img: "buy-comics-digital-comics.png",
+                    text: "digital comics",
+                },
+                {
+                    img: "buy-comics-merchandise.png",
+                    text: "dc merchandise",
+                },
+                {
+                    img: "buy-comics-subscriptions.png",
+                    text: "sebscription",
+                },
+                {
+                    img: "buy-comics-shop-locator.png",
+                    text: "comic shop locator",
+                },
+                {
+                    img: "buy-dc-power-visa.svg",
+                    text: "dc power visa",
+                },
+            ]
+        }
+    },
+    methods: {
+        getImageUrl(imgName) {
+            return new URL(`../assets/img/${imgName}`, import.meta.url).href;
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="wrapper">
+        <div class="container"> 
+            <div v-for="(icon, index) in icons" class="icon-container">
+                <img :src="getImageUrl(icon.img)" alt="">
+                <span>{{ icon.text }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../style/partials/mixin" as *;
+@use "../style/partials/variables" as *;
+
+.wrapper {
+   background-color: $primary;
+   position: relative;
+   padding: 3rem;
+   .container {
+        @include flex(row, center, center);
+        gap: 2rem;
+        color: white;
+
+        .icon-container {
+            @include flex(row, center, center);
+            gap: 1rem;
+            position: relative;
+            z-index: 3;
+        }
+
+        img {
+            height: 50px;
+        }
+   }
+   
+}
+</style>
